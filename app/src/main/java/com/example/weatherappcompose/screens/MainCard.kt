4001082d7def9492557c20f96216afd2ceb10bd4
KateSilencio <ekate.austr89@gmail.com>
2025-10-12 15:08:48 +0300

    Приложение Прогноз погоды
    Прототип программы без архитектуры и di

---
+++ app/src/main/java/com/example/weatherappcompose/screens/MainCard.kt
@@ -0,0 +1,232 @@
+package com.example.weatherappcompose.screens
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.itemsIndexed
+import androidx.compose.foundation.pager.HorizontalPager
+import androidx.compose.foundation.pager.rememberPagerState
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.TabRowDefaults.tabIndicatorOffset
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.Tab
+import androidx.compose.material3.TabRow
+import androidx.compose.material3.TabRowDefaults
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.MutableState
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.alpha
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import coil.compose.AsyncImage
+import com.example.weatherappcompose.R
+import com.example.weatherappcompose.data.WeatherModel
+import com.example.weatherappcompose.ui.theme.MyBlue
+import kotlinx.coroutines.launch
+import org.json.JSONArray
+import org.json.JSONObject
+
+//@Preview(showBackground = true)
+@Composable
+fun MainCard(
+    currentDay: MutableState<WeatherModel>,
+    onClickSync: () -> Unit,
+    onClickSearch: () -> Unit
+) {
+    Column(
+        modifier = Modifier
+            .padding(5.dp)
+    ) {
+        Card(
+            modifier = Modifier.fillMaxWidth(),
+            colors = CardDefaults.cardColors(containerColor = MyBlue),
+            elevation = CardDefaults.cardElevation(0.dp),
+            shape = RoundedCornerShape(12.dp)
+        ) {
+            Column(
+                modifier = Modifier.fillMaxWidth(),
+                horizontalAlignment = Alignment.CenterHorizontally
+            ) {
+                Row(
+                    modifier = Modifier.fillMaxWidth(),
+                    horizontalArrangement = Arrangement.SpaceBetween
+                ) {
+                    Text(
+                        modifier = Modifier.padding(top = 5.dp, start = 5.dp),
+                        text = currentDay.value.time,
+                        style = TextStyle(fontSize = 16.sp),
+                        color = Color.White
+                    )
+                    AsyncImage(
+                        modifier = Modifier
+                            .size(36.dp)
+                            .padding(top = 5.dp, end = 5.dp),
+                        model = "https:" + currentDay.value.icon,
+                        contentDescription = "weather_icon"
+                    )
+                }
+                Text(
+                    text = currentDay.value.city,
+                    style = TextStyle(fontSize = 24.sp),
+                    color = Color.White
+                )
+                Text(
+                    text = if (currentDay.value.currentTemp.isNotEmpty())
+                        currentDay.value.currentTemp.toFloat().toInt().toString() + " C"
+                    else
+                        "${currentDay.value.maxTemp.toFloat().toInt()}C/${currentDay.value.minTemp.toFloat().toInt()}C",
+                    style = TextStyle(fontSize = 66.sp),
+                    color = Color.White
+                )
+                Text(
+                    text = currentDay.value.condition,
+                    style = TextStyle(fontSize = 16.sp),
+                    color = Color.White
+                )
+                Row(
+                    modifier = Modifier.fillMaxWidth(),
+                    horizontalArrangement = Arrangement.SpaceBetween
+                ) {
+                    IconButton(
+                        onClick = {
+                            onClickSearch.invoke()
+                        }
+                    ) {
+                        Icon(
+                            modifier = Modifier.size(24.dp),
+                            painter = painterResource(id = R.drawable.search_24_ic),
+                            contentDescription = "searchIcon",
+                            tint = Color.White
+                        )
+                    }
+                    Text(
+//                        text = "${
+//                            currentDay.value.minTemp.toFloat().toInt()
+//                        } C/${currentDay.value.maxTemp.toFloat().toInt()} C",
+                        text = "UV ${currentDay.value.uv.toFloat().toInt()}",
+                        style = TextStyle(fontSize = 16.sp),
+                        color = Color.White
+                    )
+                    IconButton(
+                        onClick = {
+                            onClickSync.invoke()
+                        }
+                    ) {
+                        Icon(
+                            modifier = Modifier.size(24.dp),
+                            painter = painterResource(id = R.drawable.sync_24_ic),
+                            contentDescription = "synchronization",
+                            tint = Color.White
+                        )
+                    }
+
+                }
+            }
+        }
+
+    }
+}
+
+//@Preview(showBackground = true)
+@Composable
+fun TabLayout(
+    daysList: MutableState<List<WeatherModel>>,
+    currentDay: MutableState<WeatherModel>
+) {
+    val tabList = listOf("Hours", "Days")
+    val pagerState = rememberPagerState(pageCount = { tabList.size })
+    val tabIndex = pagerState.currentPage
+    val coroutineScope = rememberCoroutineScope()
+
+    Column(
+        modifier = Modifier
+            .padding(start = 5.dp, end = 5.dp)
+            .clip(RoundedCornerShape(5.dp))
+    ) {
+        TabRow(
+            selectedTabIndex = tabIndex,
+            indicator = { tabPositions ->
+                TabRowDefaults.SecondaryIndicator(
+                    modifier = Modifier
+                        .tabIndicatorOffset(tabPositions[pagerState.currentPage]),
+                    height = 2.dp,
+                    color = Color.White
+                )
+            },
+            containerColor = MyBlue,
+            contentColor = Color.White
+        ) {
+            tabList.forEachIndexed { index, text ->
+                Tab(
+                    selected = pagerState.currentPage == index,
+                    onClick = {
+                        coroutineScope.launch {
+                            pagerState.animateScrollToPage(index)
+                        }
+                    },
+                    text = {
+                        Text(
+                            text = text,
+                            style = TextStyle(fontSize = 16.sp)
+                        )
+                    }
+                )
+            }
+        }
+        HorizontalPager(
+            state = pagerState,
+            modifier = Modifier.weight(1.0f)
+        ) { index ->
+            val list = when (index) {
+                0 -> getWeatherbyHours(currentDay.value.hours)
+                1 -> daysList.value
+                else -> daysList.value
+            }
+            MainList(list, currentDay)
+        }
+
+    }
+}
+
+private fun getWeatherbyHours(hours: String): List<WeatherModel> {
+    if (hours.isEmpty()) {
+        return listOf()
+    }
+
+    val hoursArray = JSONArray(hours)
+    val list = ArrayList<WeatherModel>()
+    for (i in 0 until hoursArray.length()) {
+        val item = hoursArray[i] as JSONObject
+        list.add(
+            WeatherModel(
+                "",
+                item.getString("time"),
+                item.getString("temp_c").toFloat().toInt().toString() + " C",
+                item.getJSONObject("condition").getString("text"),
+                item.getJSONObject("condition").getString("icon"),
+                "",
+                "",
+                "",
+                ""
+            )
+        )
+    }
+    return list
+}
